c394a1d83c04defcecffa0fcb0f84ebf55b96d80
David <davidst96@gmail.com>
2023-11-28 09:11:08 -0500

    Cosmetic changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,23 @@ import { data as dataFetcher } from './Data.tsx'
 
 function Entry(props: any) {
   return (
-    <div onClick={() => { props.click() }} className="entry">
+    <div onClick={() => { props.click() }} className={"entry" + (props.open ? " entry-expanded" : "")}>
       <div className="entry-title">
-        {props.image ? (<img className="entry-title-image" src={props.image}></img>) : null}
+        {props.thumbnail ? (<img className="entry-title-image" src={props.thumbnail}></img>) : null}
         <div className="entry-title-text">
           <h2 className="entry-title-name"><span>{props.title}</span><span className="entry-title-year">{props.year}</span></h2>
-          <p>{props.subtitle}</p>
+          <p className="entry-title-subtitle">{props.subtitle}</p>
         </div>
       </div>
-      <div className={"entry-expandable" + (props.open ? " entry-expanded" : "")}>
+      <div className={"entry-expandable"}>
         <p>
           {props.children}
         </p>
+        <div className="entry-gallery">
+          {props.images.map((i: string) =>
+            (<img src={i}></img>)
+          )}
+        </div>
         {props.platforms !== undefined ? (<h3>Platforms</h3>) : ""}
         <p>{props.platforms}</p>
         {props.links !== undefined && props.links.length > 0 ? (<h3>Links</h3>) : ""}
@@ -37,7 +42,7 @@ function App() {
     <>
       <header>
         <h1>David Stearns</h1>
-        <p>I am a software developer, but I make things besides software, too.</p>
+        <p>Let's make something together</p>
         <div className="socials">
           <a href="mailto:dave.j.stearns@gmail.com">Email</a>
           <a href="http://linkedin.com/in/david-stearns-70727437">LinkedIn</a>
@@ -52,7 +57,8 @@ function App() {
             year={d.year}
             subtitle={d.oneline}
             platforms={d.platforms}
-            image={d.thumbnail}
+            thumbnail={d.thumbnail}
+            images={d.images}
             links={d.links.map((a) => (<a href={a.link}>{a.title}</a>))}
             open={open[i]}
             click={() => closeAll(i)}
--- src/Data.tsx
+++ src/Data.tsx
@@ -1,22 +1,22 @@
 export const data = function(){
     return [
-        {
-            name: "Ocean game (TEMP NAME)",
-            year: "In progress",
-            oneline: "An rpg",
-            description: "",
-            images: "",
-            thumbnail: "",
-            platforms: ["windows","mac"],
-            links: [],
-        },
+        // {
+        //     name: "Ocean game (TEMP NAME)",
+        //     year: "In progress",
+        //     oneline: "An rpg",
+        //     description: "",
+        //     images: "",
+        //     thumbnail: "",
+        //     platforms: ["windows","mac"],
+        //     links: [],
+        // },
         {
             name: "Rose Curve Visualizer",
             year: "2023",
-            oneline: "A mesmerizing garden of animated mathematical roses, inviting exploration with playful and intuitive controls.",
+            oneline: "An interactive experience that blends mathematics and art",
             description: "This online garden transforms mathematical beauty into a fun and mesmerizing experience. Centered around the idea of exploration and play, the controls are designed to let curiosity guide the user to explore a variety of animated rose curves.",
-            images: "",
-            thumbnail: "",
+            images: ["DivineLawTitle.png","DivineLawScreenshot.png"],
+            thumbnail: "DivineLawTitle.png",
             platforms: ["web"],
             links: [
                 {title:"View now", link:"https://davetheknave.github.io/Rose-Curve-Visualizer"},
@@ -27,41 +27,54 @@ export const data = function(){
             name: "Personal Portfolio",
             year: "2023",
             oneline: "You're looking at it!",
-            description: ``,
-            images: "",
-            thumbnail: "",
+            description: `My personal portfolio is designed to give a quick overview of some of the personal projects I've done, with more detailed information being available on GitHub or elsewhere. My latest website is similar to my previous one, but with a few notable improvements showcasing what I've learned:
+            - It now has a back end to allow me to update it easily.
+            - Having a single page without routing allows for everything to be visible at a glance. Details are hidden until the user decides to click on a project.
+            - The portfolio is centered around larger images. This is more eye catching and reflects on the visual nature of much of my work.
+            - The design is more modern and uses animations to gently guide the user.`,
+            images: ["DivineLawTitle.png","DivineLawScreenshot.png"],
+            thumbnail: "DivineLawTitle.png",
             platforms: ["web"],
-            links: [{"title":"Source Code","link":"https://github.com/davetheknave"}],
+            links: [{"title":"Source Code","link":"https://github.com/davetheknave/Web-Portfolio-03"}],
         },
         {
             name: "Divine Law",
             year: "2021",
-            oneline: "My and my sister's entry to AI and Games Jam 2021. This is a video game about an angel that defies the laws of physics to stop a serpent from breaking into the Garden of Eden.",
-            description: ``,
-            images: "",
-            thumbnail: "noimage.png",
+            oneline: "My and my sister's entry to AI and Games Jam 2021",
+            description: `My sister and I had one week to create a video game for the AI and Games Jam of 2021 with the theme of "breaking the rules". We designed and created a puzzle game about an angel that can break the laws of physics. We won 7th place out of over 100 entries.`,
+            images: ["DivineLawTitle.png","DivineLawScreenshot.png"],
+            thumbnail: "DivineLawTitle.png",
             platforms: ["windows","mac"],
-            links: [{title:"Play now",link:"http://itch.io"}],
+            links: [{title:"Play now",link:"https://leafyline.itch.io/divine"}],
         },
         {
             name: "Old portfolio sites",
             year: "",
-            oneline: "Previous versions of my portfolio. Kept here for digital archaeologists.",
-            description: "They aren't hosted anymore, but the source code is still available.",
-            images: "",
-            thumbnail: "",
+            oneline: "Previous versions of my portfolio. Kept here for digital archaeologists",
+            description: `The first personal site I made soon after I graduated. It featured fun animations and was artistic, but also a bit complicated.
+            The second one is a simpler site with a consistent header on the left side. It improved upon the previous site in many ways:
+            - The simpler look, while less artistic, was more robust and responsive to work on more devices.
+            - The soft colors are easier on the eyes.
+            - The header on the left has contact information and serves as a persistent call to action.`,
+            images: ["website2.png","website1.png"],
+            thumbnail: "website2.png",
             platforms: ["web"],
-            links: [{title: "Source code", link: "https://github.com/davetheknave"}],
+            links: [
+                {title: "Source code for second site", link: "https://github.com/davetheknave/davetheknave.github.io/"},
+                {title: "Source code for first site", link: "https://github.com/davetheknave/Web-Portfolio-01"},
+            ],
         },
         {
             name: "D-Chess",
             year: "2018",
-            oneline: "A five-dimensional chess game for two players that involves moving pieces into other dimensions and into the future.",
-            description: "",
-            images: "",
-            thumbnail: "",
+            oneline: "A futuristic chess game for two players featuring time travel and an alternate dimension",
+            description: `One day I decided to make a video game about multi-dimensional chess. I tried to cram as many alternate dimensions into the rules of chess as I could without making the game too difficult for humans to play. I ended up creating a game with two additional dimensions: one time, and the other an alternate physical space.
+
+            As this game does not feature an AI opponent, this is best played with 2 players.`,
+            images: ["DChess.png"],
+            thumbnail: "DChess.png",
             platforms: ["windows","mac","linux"],
-            links: [{title: "Source code", link: "https://github.com/davetheknave"}],
+            links: [{title: "Source code", link: "https://github.com/davetheknave/D-Chess/"}],
         },
     ]
 }
