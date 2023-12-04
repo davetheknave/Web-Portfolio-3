ebf7785b936b5f797de6919abb617101a839ec4b
David Stearns <knavedavid@gmail.com>
2023-12-04 13:21:44 -0500

    Make routing work

--- src/App.tsx
+++ src/App.tsx
@@ -13,38 +13,45 @@ function App() {
   function getSlug(title: string) {
     return title.replace(/[^A-Za-z0-9_]/g, "-");
   }
-  const router = createBrowserRouter([
-    {
-      path: "/",
+  const routes: any = data.map((d, i) => {
+    return {
+      path: "/" + getSlug(d.name),
       element:
-        <div id="gallery" className={"lg:relative w-full top-32 flex flex-col 2xl:flex-row flex-wrap gap-16 justify-center" + ("description" in currentDataEntry ? " hidden" : "")}>
-          {data.map((d, i) => (
-            <ImageEntry
-              key={i}
-              title={d.name}
-              slug={getSlug(d.name)}
-              year={d.year}
-              subtitle={d.oneline}
-              platforms={d.platforms}
-              thumbnail={d.thumbnail}
-              images={d.images}
-              links={d.links.map((a, ii) => (<a key={ii} href={a.link}>{a.title}</a>))}
-            >
-              {d.description}
-            </ImageEntry>
-          ))}
-        </div>,
-        errorElement: <ErrorPage />
-    },
-  ]);
+        (
+          <BigView key={i} className={("description" in d ? "" : " hidden")}
+            data={d}
+          />
+        )
+    }
+  })
+  routes.push({
+    path: "/",
+    element:
+      <div id="gallery" className={"lg:relative w-full top-32 flex flex-col 2xl:flex-row flex-wrap gap-16 justify-center" + ("description" in currentDataEntry ? " hidden" : "")}>
+        {data.map((d, i) => (
+          <ImageEntry
+            key={i}
+            title={d.name}
+            slug={getSlug(d.name)}
+            year={d.year}
+            subtitle={d.oneline}
+            platforms={d.platforms}
+            thumbnail={d.thumbnail}
+            images={d.images}
+            links={d.links.map((a, ii) => (<a key={ii} href={a.link}>{a.title}</a>))}
+          >
+            {d.description}
+          </ImageEntry>
+        ))}
+      </div>,
+    errorElement: <ErrorPage />
+  })
+  const router = createBrowserRouter(routes);
   return (
     <>
       <Header bio={`I am a designer, developer, and artist. My passion is in creating experiences inspired by the world around me. I am perpetually learning, constantly expanding my skill set, so that I can continue punching above my weight.`} />
       <div id="page" className="lg:absolute left-64 pl-12 pr-12 w-full">
         <RouterProvider router={router} />
-        <BigView className={("description" in currentDataEntry ? "" : " hidden")}
-          data={currentDataEntry}
-        />
       </div>
     </>
   )
